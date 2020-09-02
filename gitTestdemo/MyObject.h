05a51826ade9204983e10565342696619617f55e
jinfeng <851406283@qq.com>
2020-09-02 09:50:54 +0800

    添加MyObject

---
+++ gitTestdemo/MyObject.h
@@ -0,0 +1,17 @@
+//
+//  MyObject.h
+//  gitTestdemo
+//
+//  Created by jinfeng on 2020/9/2.
+//  Copyright © 2020 jinfeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ gitTestdemo/MyObject.m
@@ -0,0 +1,13 @@
+//
+//  MyObject.m
+//  gitTestdemo
+//
+//  Created by jinfeng on 2020/9/2.
+//  Copyright © 2020 jinfeng. All rights reserved.
+//
+
+#import "MyObject.h"
+
+@implementation MyObject
+
+@end
